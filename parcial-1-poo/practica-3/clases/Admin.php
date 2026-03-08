810d03618ad77c597c044787e994afdea784e523
jesus-zat <jesusitojzt@gmail.com>
2026-03-08 14:23:08 -0700

    Agregamos clase Admin que hereda de Usuario

---
+++ parcial-1-poo/practica-3/clases/Admin.php
@@ -0,0 +1,15 @@
+<?php
+//incluimos la clase usuario para heredar sus propiedades
+/*Utilizamos reuire_once para asegurarnos que la clase Usuario
+esta disponinble y evitar errores en dado caso que se haya
+incluido antes, ya que Admin hereda de ella 
+*/
+require_once "Usuario.php";
+
+class Admin extends Usuario {
+    //Metodo original de Admin
+    public function getRol(){
+        return "Administrador"
+    }
+
+    }
