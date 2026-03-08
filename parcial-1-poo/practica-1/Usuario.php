a6d734527a2c94e47ef3188da3368f43e036d210
jesus-zat <jesusitojzt@gmail.com>
2026-03-07 22:11:48 -0700

    Agregamos clase usuario, con constructor, getters y setters

---
+++ parcial-1-poo/practica-1/Usuario.php
@@ -0,0 +1,36 @@
+<?php 
+Class Usuario {
+    //declaramos atributos privados
+    private $nombre;
+    private $correo;
+
+    //constructor de la clase
+    public function __construct($nombre, $correo)
+    {
+        $this->nombre = $nombre;
+        $this->correo = $correo;
+    }
+
+    //metodos getters
+
+    public function getNombre() {
+        return $this->nombre;
+
+    }
+
+    public function getCorreo() {
+        return $this->correo;
+    }
+
+    //metodos setters
+
+    public function setNombre($nombre) {
+        $this->nombre = $nombre;
+    }
+  
+    public function setCorreo($correo) {
+        $this->correo = $correo;
+    }
+
+}
+
