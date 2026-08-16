9c225e1b2a4cd5efceda4b9df44132c8a63b29e0
jesus-zat <jesusitojzt@gmail.com>
2026-08-15 22:28:15 -0700

    Subimos estructura de proyecto y tambien archivo de conexion a la base de datos

---
+++ parcial-3-mvc/examen-practico/config/DataBase.php
@@ -0,0 +1,31 @@
+<?php
+    //Crear una clase para conexion a base de datos mediante PDO.
+
+    class DataBase{
+        //Atributos de la clase DataBase
+        private $host="localhost";
+        private $db="proyecto";
+        private $user="demo";
+        private $password = "123";
+
+        public function __construct()
+        {
+            //Constructor...
+
+        }
+
+        //Metodo para conexion a la base de datos.
+        public function connect(){
+            try {
+                $PDO = new PDO("mysql:host=".$this->host.
+                ";dbname=".$this->db,$this->user,$this->password);
+                 return $PDO;
+
+            } catch (PDOException $E) {
+                 return $e->getMessage();  
+
+            }
+        }
+
+    }
+?>
